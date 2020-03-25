62055ade06d690ffebb9a41979a88afa004a49e0
teixeiramarcia <filipa.teixeira98@gmail.com>
2020-03-25 12:38:57 +0000

    Dados para carregamento

--- grupo34/projC/src/main.c
+++ grupo34/projC/src/main.c
@@ -1,8 +1,12 @@
 #include "sgv.h"
+#include <stdio.h>
 
 int main(int argc, const char* argv[]) {
+    if(argc < 2) {
+        printf("provide Data folder\n");
+        return 1;
+    }
     SGV sgv = initSGV();
-
     loadSGVFromFiles(sgv, argv[1]);
 
     //getProductsStartedByLetter(sgv, 'A'); //query 2
@@ -13,9 +17,9 @@ int main(int argc, const char* argv[]) {
 
     //getProductsBoughtByClient(sgv, "A3984"); //query 7
 
-    //getSalesAndProfit(sgv, 1, 1); //query 8
+    //getSalesAndProfit(sgv, 1, 12); //query 8
 
-    //getProductBuyers(sgv, "AF1184", 1); //query 9
+    //getProductBuyers(sgv, "NR1091", 3); //query 9
 
     destroySGV(sgv);
     return 0;
